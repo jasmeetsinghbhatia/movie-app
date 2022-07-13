0ab387940e3d4022f37198172ecc59bbd440d750
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2022-07-13 12:55:12 -0400

    Updates

---
+++ precache-manifest.80bf3ceb25b4137afdd7a1ec86afe45e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9baf62fcd03b48ad9560",
+    "url": "/movie-app/static/css/main.48d11d71.chunk.css"
+  },
+  {
+    "revision": "9baf62fcd03b48ad9560",
+    "url": "/movie-app/static/js/main.9baf62fc.chunk.js"
+  },
+  {
+    "revision": "6b65661639b2a58d817b",
+    "url": "/movie-app/static/css/1.e7d8a1b2.chunk.css"
+  },
+  {
+    "revision": "6b65661639b2a58d817b",
+    "url": "/movie-app/static/js/1.6b656616.chunk.js"
+  },
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "37dfadee4851fa20ac5067011ccaf9d9",
+    "url": "/movie-app/index.html"
+  }
+];
